3158cccf4cff7503cbaab721d2127065ed46aa74
coldJune <785691557@qq.com>
2020-07-28 19:44:02 +0800

    Spring IoC容器BeanFacotry和ApplicationContext使用

---
+++ thinkInSpring/ioc-container-overview/src/main/java/com/jun/ioc/overview/container/AnnotationApplicationContextAsIoCContainer.java
@@ -0,0 +1,54 @@
+package com.jun.ioc.overview.container;
+
+import com.jun.ioc.overview.dependency.domain.Book;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/** 注解能力{@link ApplicationContext}作为IoC  容器示例
+ * @Author dengxiaojun
+ * @Date 2020/7/27 22:26
+ * @Version 1.0
+ */
+public class AnnotationApplicationContextAsIoCContainer {
+    public static void main(String[] args) {
+        //创建BeanFactory容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        //将当前类AnnotationApplicationContextAsIoCContainer作为配置类(Configuration Class)
+        applicationContext.register(AnnotationApplicationContextAsIoCContainer.class);
+        //启动应用上下文
+        applicationContext.refresh();
+        //依赖查找集合对象
+        lookupCollectionType(applicationContext);
+    }
+
+    /**
+     * 通过java注解的方式定义了一个Bean
+     * @return
+     */
+    @Bean
+    public Book book(){
+        Book book = new Book();
+        book.setPrice(5);
+        book.setName("spring");
+        return book;
+    }
+    /**
+     * 根据类型查找集合对象
+     * @param beanFactory
+     */
+    private static void lookupCollectionType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory){
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory)beanFactory;
+            Map<String, Book> books = listableBeanFactory.getBeansOfType(Book.class);
+            System.out.println("查找到所有的Book对象："+books);
+        }
+    }
+}
---
+++ thinkInSpring/ioc-container-overview/src/main/java/com/jun/ioc/overview/container/BeanFactoryAsIoCContainer.java
@@ -0,0 +1,40 @@
+package com.jun.ioc.overview.container;
+
+import com.jun.ioc.overview.dependency.domain.Book;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import java.util.Map;
+
+/** {@link BeanFactory}作为IoC  容器示例
+ * @Author dengxiaojun
+ * @Date 2020/7/27 22:26
+ * @Version 1.0
+ */
+public class BeanFactoryAsIoCContainer {
+    public static void main(String[] args) {
+        //创建BeanFactory容器
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        //加载配置
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        //XML 配置文件 classPath路径
+        String location = "classpath:META-INF/dependency-lookup-context.xml";
+        int beanDefinitionsCount = reader.loadBeanDefinitions(location);
+        System.out.println("Bean定义加载数量："+beanDefinitionsCount);
+        //依赖查找集合对象
+        lookupCollectionType(beanFactory);
+    }
+    /**
+     * 根据类型查找集合对象
+     * @param beanFactory
+     */
+    private static void lookupCollectionType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory){
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory)beanFactory;
+            Map<String, Book> books = listableBeanFactory.getBeansOfType(Book.class);
+            System.out.println("查找到所有的Book对象："+books);
+        }
+    }
+}
