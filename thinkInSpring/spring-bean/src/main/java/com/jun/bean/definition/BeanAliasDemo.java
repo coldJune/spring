b4c0671d4666dc17d1b13cb368939afa05dc43f4
coldJune <785691557@qq.com>
2020-08-17 22:37:05 +0800

    Bean的命名和别名

---
+++ thinkInSpring/spring-bean/src/main/java/com/jun/bean/definition/BeanAliasDemo.java
@@ -0,0 +1,21 @@
+package com.jun.bean.definition;
+
+import com.jun.ioc.overview.dependency.domain.Book;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/** Bean别名示例
+ * @Author dengxiaojun
+ * @Date 2020/8/17 22:33
+ * @Version 1.0
+ */
+public class BeanAliasDemo {
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/bean-definitions-context.xml");
+        Book book = beanFactory.getBean("book", Book.class);
+        //通过别名myBook获取曾用名book的Bean
+        Book myBook = beanFactory.getBean("myBook",Book.class);
+        System.out.println("myBook是否和book相同："+(book == myBook));
+
+    }
+}
