dcbaec679a7c95d33217796a3f0c26536cda6e8a
dengxiaojun-001 <dengxiaojun-001@cpic.com.cn>
2020-12-07 14:45:55 +0800

    [thinkInSpring]Spring Bean生命周期 Aware接口回调阶段

--- thinkInSpring/bean-lifecycle/src/main/java/com/jun/bean/lifecycle/BeanInstantiationLifecycleDemo.java
+++ thinkInSpring/bean-lifecycle/src/main/java/com/jun/bean/lifecycle/BeanInstantiationLifecycleDemo.java
@@ -8,6 +8,7 @@ import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
@@ -21,9 +22,17 @@ import org.springframework.util.ObjectUtils;
  */
 public class BeanInstantiationLifecycleDemo {
     public static void main(String[] args) {
+        executeBeanFactory();
+        System.out.println("---------");
+        executeApplicationContext();
+
+    }
+
+    private static void executeBeanFactory(){
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
-        // 追加 BeanPostProcessor实现
+        // 方法一：追加 BeanPostProcessor实现 MyInstantiationAwareBeanPostProcessor
+        // 方法二： 将MyInstantiationAwareBeanPostProcessor作为Bean注册
         beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
         // 基于XML 资源BeanDefinitionReader实现
         XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
@@ -40,63 +49,30 @@ public class BeanInstantiationLifecycleDemo {
         // 构造器注入按照类型注入 resolveDependency
         BookHolder bookHolder = beanFactory.getBean("bookHolder",BookHolder.class);
         System.out.println(bookHolder);
-
     }
 
-    static class MyInstantiationAwareBeanPostProcessor
-            implements InstantiationAwareBeanPostProcessor{
-        @Override
-        public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
-            if(ObjectUtils.nullSafeEquals("cheapBook",beanName)
-                    && CheapBook.class.equals(beanClass)){
-                // 将配置完成的CheapBook 覆盖
-                return new CheapBook();
-            }else{
-                return null; // 保持Spring IoC的实例化操作
-            }
-        }
+    private static void executeApplicationContext(){
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext();
 
-        @Override
-        public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
-            if(ObjectUtils.nullSafeEquals("book",beanName)
-                    && Book.class.equals(bean.getClass())){
-                // "boook"对象不允许属性赋值(填入)(配置元信息->属性值)
-                Book book = (Book) bean;
-                book.setName("coldjune_");
-                return false;
-            }
-            return true;
-        }
+        // 追加 BeanPostProcessor实现 MyInstantiationAwareBeanPostProcessor
+        String[] locations = {"META-INF/dependency-lookup-context.xml","META-INF/bean-constuctor-rdependency-injection-context.xml"};
+        applicationContext.setConfigLocations(locations);
+        // 启动应用上下文
+        applicationContext.refresh();
+        // 基于XML 资源BeanDefinitionReader实现
 
-        // book 是跳过 Bean 属性赋值填入
-        // cheapBook 是完全跳过 Bean 实例化(Bean 属性赋值填入)
-        // bookHolder
-        @Override
-        public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
-            // 对“bookHolder” Bean进行拦截
-            if(ObjectUtils.nullSafeEquals("bookHolder",beanName)
-                    && BookHolder.class.equals(bean.getClass())){
-                // 假设 <property name="number" value="1"/> 配置的话，那么在PropertyValues就包含一个 PropertyValue(number=1)
-                final MutablePropertyValues mutablePropertyValues;
-                if( pvs instanceof MutablePropertyValues){
-                    mutablePropertyValues = (MutablePropertyValues) pvs;
-                }else{
-                    mutablePropertyValues = new MutablePropertyValues();
-                }
-                // 等价于 <property name="number" value="1"/>
-                mutablePropertyValues.addPropertyValue("number","1");
-                // 如果存在“desc”的字段配置 则把值进行替换
-                if(mutablePropertyValues.contains("desc")){
-                    // 原始配置 <property name="desc" value="the bookHolder v1"/>
-                    // MutablePropertyValues 是不可变的 即不能直接替换
-                    mutablePropertyValues.removePropertyValue("desc");
-                    mutablePropertyValues.addPropertyValue("desc", "the bookHolder v2");
-                }
-                return mutablePropertyValues;
-            }
 
-            return null;
-        }
+        Book book =applicationContext.getBean("book", Book.class);
+        System.out.println(book);
+
+        Book cheapBook = applicationContext.getBean("cheapBook",Book.class);
+        System.out.println(cheapBook);
+
+        // 构造器注入按照类型注入 resolveDependency
+        BookHolder bookHolder = applicationContext.getBean("bookHolder",BookHolder.class);
+        System.out.println(bookHolder);
+        // 关闭应用上下文
+        applicationContext.close();
     }
 
 }
--- thinkInSpring/bean-lifecycle/src/main/java/com/jun/bean/lifecycle/BookHolder.java
+++ thinkInSpring/bean-lifecycle/src/main/java/com/jun/bean/lifecycle/BookHolder.java
@@ -1,17 +1,50 @@
 package com.jun.bean.lifecycle;
 
 import com.jun.ioc.overview.domain.Book;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
 /**
  * @Author dengxiaojun
  * @Date 2020/12/2 21:42
  * @Version 1.0
  */
-public class BookHolder {
+public class BookHolder implements BeanNameAware, BeanClassLoaderAware, BeanFactoryAware, EnvironmentAware {
     private Book book;
 
     private Integer number;
     private String desc;
+
+    private ClassLoader classLoader;
+    private BeanFactory beanFactory;
+    private String beanName;
+
+    private Environment environment;
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
     public BookHolder(Book book) {
         this.book = book;
     }
